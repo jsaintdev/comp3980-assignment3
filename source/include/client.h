a500325c4e49c10fa2ad43bddf90ad5e2684bb10
justinsaint <jsaintdev@gmail.com>
2024-11-01 19:17:25 -0700

    Set up usage function

--- source/include/client.h
+++ source/include/client.h
@@ -20,10 +20,10 @@
 #define FIFO_OUT "./fifo_out"
 #define BUFFER_SIZE 1024
 #define FILTER_SIZE 64
-#define ARGS_NUM 5
+#define ARGS_NUM 7
 
 static void parse_arguments(int argc, char *argv[], char **input_string, char filter[FILTER_SIZE]);
 static int  handle_arguments(const char *input_string, const char *filter);
-static void usage(const char *program_name, const char *message);
+_Noreturn static void usage(const char *program_name, int exit_code, const char *message);
 
 #endif //CLIENT_H
--- source/src/client.c
+++ source/src/client.c
@@ -1,3 +1,40 @@
 //
 // Created by justinsaint on 01/11/24.
 //
+
+include "client.h"
+
+int main(int argc, char *argv[])
+{
+    char                   *address;
+    char                   *port_str;
+    in_port_t               port;
+    int                     sockfd;
+    struct sockaddr_storage addr;
+
+    address  = NULL;
+    port_str = NULL;
+    parse_arguments(argc, argv, &address, &port_str);
+    handle_arguments(argv[0], address, port_str, &port);
+    convert_address(address, &addr);
+    sockfd = socket_create(addr.ss_family, SOCK_STREAM, 0);
+    socket_connect(sockfd, &addr, port);
+    socket_close(sockfd);
+
+    return EXIT_SUCCESS;
+}
+
+_Noreturn static void usage(const char *program_name, int exit_code, const char *message)
+{
+    if(message)
+    {
+        fprintf(stderr, "%s\n", message);
+    }
+
+    fprintf(stderr, "Usage: %s [-h] -s <string> -f [upper lower null] <address> <port>\n", program_name);
+    fputs("Options:\n", stderr);
+    fputs("  -h  Display this help message\n", stderr);
+    fputs("  -s\t <string>  Input string to be converted\n", stderr);
+    fputs("  -f\t <filter>  Choose from: [upper, lower, null]  \n", stderr);
+    exit(exit_code);
+}
